dc415228ce45677ab9b05d7d9bacbb3c6bd56a6b
Barney Gammond <barney@gammond.co.uk>
2021-06-21 19:31:56 +0100

    Fix: Changed CSS classes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7a5b04d36af61fa20b5a92351abc1b08');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1695fcbd7a5ecc34d0daa517b6ef1eca');
